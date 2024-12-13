c89641d8207b26d9bb96810e0362d05c4cb96162
Semini-Sawbhagya <161110096+Semini-Sawbhagya@users.noreply.github.com>
2024-12-13 12:21:16 +0530

    mysql2 installed

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,35 @@
 import express from 'express';
+import mysql from 'mysql';
 
 const app = express();
+
+const db = mysql.createConnection({
+  host: 'localhost',
+  user: "root",
+  password: "123456",
+  database: "bank_database",
+});
+
+// Check if the database connection is successful
+db.connect((err) => {
+  if (err) {
+    console.error('Error connecting to the database:', err);
+    return;
+  }
+  console.log('Connected to the database successfully!');
+});
+
+app.get("/account", (req, res) => {
+  const query = "SELECT * FROM account";
+  db.query(query, (err, result) => {
+    if (err) {
+      console.log(err);
+      res.status(500).send('Error querying database');
+      return;
+    }
+    res.json(result);
+  });
+});
 
 // A simple route to test if the server is running
 app.get('/', (req, res) => {
