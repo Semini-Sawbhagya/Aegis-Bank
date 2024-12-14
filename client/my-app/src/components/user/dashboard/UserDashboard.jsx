79c33657e620476937582f9e8a7c1de7607ef5e6
Semini-Sawbhagya <161110096+Semini-Sawbhagya@users.noreply.github.com>
2024-12-14 23:03:35 +0530

    Do changes to userdashboard

--- client/my-app/src/components/user/dashboard/UserDashboard.jsx
+++ client/my-app/src/components/user/dashboard/UserDashboard.jsx
@@ -1,31 +1,34 @@
 import React from 'react'
 import backgroundImage from '../../../assets/background.jpg';
-import logo from '../../../assets/AegisLogo.png';
-import home from '../../../assets/home.svg';
-import moneytransfer from '../../../assets/moneytransfer.svg';
-import transactionhistory from '../../../assets/transactionhistory.svg';
-import loans from '../../../assets/loans.svg';
-import account from '../../../assets/account.svg';
+import logo from '../../../assets/image-40.png';
+import settings from '../../../assets/settings0.svg';
+import logout from '../../../assets/logout-020.svg';
+import help from '../../../assets/help0.svg';
+import trans from '../../../assets/wallet-020.svg';
+import loans from '../../../assets/bag-040.svg';
+import account from '../../../assets/bank-note-010.svg';
+import money from '../../../assets/currency-coin-dollar0.svg';
+import home from '../../../assets/home-010.svg';
 import './UserDashboard.css'
 const UserDashboard = () => {
   return (
     <div>
       <div class="desktop-1">
        <div class="rectangle-6"></div>
-       <img className="image-4" src={logo} alt="logo" />
+       <img className="image-4" src={logo}/>
        <div class="rectangle-7"></div>
        <img className="image-6" src={backgroundImage} alt="Background" />
 
        <div class="frame-15">
-       <img class="settings" src="settings0.svg" />
+       <button><img className="settings"  src={settings}/></button>
        <div class="settings2">settings</div>
     </div>
       <div class="frame-34">
-        <img class="logout-02" src="logout-020.svg" />
+        <img className="logout-02" src={logout} />
         <div class="logout">logout</div>
       </div>
       <div class="frame-22">
-        <img class="help" src="help0.svg" />
+        <img className="help" src={help} />
         <div class="help2">help</div>
       </div>
       <div class="rectangle-8"></div>
@@ -34,7 +37,7 @@ const UserDashboard = () => {
       <div class="rectangle-11"></div>
       <div class="rectangle-12"></div>
       <div class="frame-27">
-        <img class="home-01" src="home-010.svg" />
+        <img className="home-01" src={home} />
       </div>
       <div class="text">
         <br />
@@ -47,14 +50,13 @@ const UserDashboard = () => {
       <div class="transaction">TRANSACTION</div>
       <div class="history">HISTORY</div>
       <div class="loans">LOANS</div>
-      <img className="image-6" src={backgroundImage} alt="Background" />
-    <img className="bank-note-01" src={home} alt="home" />
-    <img className="currency-coin-dollar" src={moneytransfer}  alt="moneytransfer" />
+    <img className="bank-note-01" src={account} />
+    <img className="currency-coin-dollar" src={money} />
       <div class="frame-20">
-        <img className="wallet-02" src={transactionhistory} alt="transactionhistory"/>
+        <img className="wallet-02" src={trans} />
       </div>
       <div class="frame-16">
-        <img className="bag-04" src={loans} alt="loans" />
+        <img className="bag-04" src={loans} />
       </div>
       <div class="aegis-bank">AEGIS BANK</div>
   </div>
