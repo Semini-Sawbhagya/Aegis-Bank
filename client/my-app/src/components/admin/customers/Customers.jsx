1b9da16c084e85c7d6a27890d1f5aabfb4c6734f
Semini-Sawbhagya <161110096+Semini-Sawbhagya@users.noreply.github.com>
2024-12-26 22:07:52 +0530

    Customers.jsx added

---
+++ client/my-app/src/components/admin/customers/Customers.jsx
@@ -0,0 +1,42 @@
+import React from 'react';
+import { Outlet, Link } from 'react-router-dom';
+import { Tabs, Tab, Box } from '@mui/material';
+
+const Reports = () => {
+  const [value, setValue] = React.useState(0);
+
+  const handleChange = (event, newValue) => {
+    setValue(newValue);
+  };
+
+  return (
+    <div className="reports-container">
+      <Box sx={{ borderBottom: 1, borderColor: 'divider', display: 'flex', justifyContent: 'center' }}>
+        {/* Customizing color of tabs */}
+        <Tabs 
+          value={value} 
+          onChange={handleChange} 
+          aria-label="customer tabs"
+          centered
+          sx={{
+            '& .MuiTabs-indicator': {
+              backgroundColor: '#695CFE', // Change the indicator color
+            },
+            '& .MuiTab-root': {
+              color: '#888888', // Color for inactive tabs
+            },
+            '& .Mui-selected': {
+              color: '#5648CC', // Color for active tab
+            },
+          }}
+        >
+          <Tab label="Get Customer Details" component={Link} to="get-customer-details" />
+          <Tab label="Add New Customer" component={Link} to="add-new-customer" />
+        </Tabs>
+      </Box>
+      <Outlet />
+    </div>
+  );
+};
+
+export default Reports;
