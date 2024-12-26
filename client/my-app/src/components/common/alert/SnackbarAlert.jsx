8e072059bfeb27bcce54dd191f7010f13901da70
Semini-Sawbhagya <161110096+Semini-Sawbhagya@users.noreply.github.com>
2024-12-26 23:40:23 +0530

    SnackbarAlerr.jsx added

---
+++ client/my-app/src/components/common/alert/SnackbarAlert.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import Snackbar from '@mui/material/Snackbar';
+import Alert from '@mui/material/Alert';
+
+const SnackbarAlert = ({ open, onClose, severity, message }) => {
+  return (
+    <Snackbar open={open} autoHideDuration={6000} onClose={onClose}>
+      <Alert onClose={onClose} severity={severity} variant="filled" sx={{ width: '100%' }}>
+        {message}
+      </Alert>
+    </Snackbar>
+  );
+};
+
+export default SnackbarAlert;
+
