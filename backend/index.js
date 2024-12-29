c3db6fa326adf83d5a956bb9fe8074420fccbca5
Semini-Sawbhagya <161110096+Semini-Sawbhagya@users.noreply.github.com>
2024-12-29 23:10:46 +0530

    modified

--- backend/index.js
+++ backend/index.js
@@ -7,25 +7,24 @@ import protectedRoutes from './routes/protected.js';
 import refreshRoutes from './routes/refresh.js';
 import authenticateToken from './middleware/auth.js';
 import staffServices from './services/Staff/staff_services.js';
-/*import managerRoutes from './routes/m-loans.js';*/
+import managerRoutes from './routes/m-loans.js';
 
 import { getPendingLoans } from './services/ManagerLoans/manager-loans.js';   
-/*
-import { employee_loans } from './services/EmployeeLoans/employee-loans.js';*/
+import { employee_loans } from './services/EmployeeLoans/employee-loans.js';
 import { getLoanDetails, getCreditLimit, applyLoan, payLoanInstallment, getInstallmentAmount } from './services/Loan/loan_services.js';
 import { money_transfer } from './services/MoneyTransfer/money_transfer.js';
 import { getAccounts,getUserAccounts, getAccountSummary } from './services/AccountManagement/account_details.js';
-/*import { addIndividualCustomer, addOrganizationCustomer, getCustomerDetails } from './services/Customers/customer_services.js';
-import { getTransactionReport, getLateLoanPaymentReport } from './services/Reports/report_services.js';*/
+import { addIndividualCustomer, addOrganizationCustomer, getCustomerDetails } from './services/Customers/customer_services.js';
+import { getTransactionReport, getLateLoanPaymentReport } from './services/Reports/report_services.js';
 import { depositFunds, withdrawFunds,getRecentTransactions,getTransactionsHistory } from './services/Transactions/transaction_services.js';
-/*import { addEmployee, removeEmployee,updateEmployeeDetails, updateUserDetails, updateEmployeeBranch } from './services/emplyees/employee_services.js';*/
+import { addEmployee, removeEmployee,updateEmployeeDetails, updateUserDetails, updateEmployeeBranch } from './services/employees/employee_services.js';
 
 //import {addEmployee} from './services/emplyees/employee_services.js'
 
 import { getAccountDetails } from './services/Accounts/account_services.js';    
-/*import { logout } from './services/Authentication/logout.js';
+import { logout } from './services/Authentication/logout.js';
 
-import { getSavingsAccounts, createFixedDeposit } from './services/Staff/FixedDeposits/fixedDeposit.js';*/
+import { getSavingsAccounts, createFixedDeposit } from './services/Staff/FixedDeposits/fixedDeposit.js';
 import { getUserInfo, updateUserInfo, getStaffInfo, updateStaffInfo,changeUserPassword } from './services/User/user_services.js';
 
 
@@ -50,12 +49,13 @@ app.listen(PORT, () => {
     console.log(`Connected to backend! Server is running on http://localhost:${PORT}`);
 });
 
+
 // Logout route
 app.use('/auth', authRoutes);
 app.use('/api', authenticateToken, protectedRoutes);
 app.use('/refresh', refreshRoutes);
 app.use('/staff', staffServices);
-/*app.use('/manager', managerRoutes);*/
+app.use('/manager', managerRoutes);
 
 // Define routes using async functions
 app.get("/accounts", getAccounts);
@@ -63,20 +63,23 @@ app.get("/user_accounts", getUserAccounts);
 app.get("/accounts_summary", getAccountSummary);
 
 app.get("/loan_details", getLoanDetails);
-/*app.get("/credit-limit", getCreditLimit);
-app.get("/savings_accounts", getSavingsAccounts);*/
+app.get("/credit-limit", getCreditLimit);
+app.get("/savings_accounts", getSavingsAccounts);
 
 app.get("/recent_transactions/:customerId", getRecentTransactions);
 app.get("/transaction_History/:customerId", getTransactionsHistory);
+
 app.get("/manager-loans", getPendingLoans);
+
+
 
 // User info
 app.get("/user_info/:userId", getUserInfo);
 app.put("/user_info/:userId", updateUserInfo);
-/*
+
 // Staff info
 app.get("/staff_info/:userId", getStaffInfo);
-app.put("/staff_info/:userId", updateStaffInfo);*/
+app.put("/staff_info/:userId", updateStaffInfo);
 
 // Change password
 app.put("/change_password/:userId", changeUserPassword);
@@ -87,7 +90,6 @@ app.post("/pay_loan", payLoanInstallment);
 app.get("/installment", getInstallmentAmount);
 
 // Reports
-/*
 app.post("/report/transaction", getTransactionReport);
 app.post("/report/late_loan_payment", getLateLoanPaymentReport);
 
@@ -112,16 +114,16 @@ app.post("/updateStaffBranch",updateEmployeeBranch);
 app.post("/customer-details", getCustomerDetails);
 
 //logout
-app.post("/logout", logout);*/
+app.post("/logout", logout);
 // Transactions
 app.post("/deposit", depositFunds);
 app.post("/withdraw", withdrawFunds);
 app.post("/money_transfer", money_transfer);
-/*app.post("/employee_loans",employee_loans);*/
+app.post("/employee_loans",employee_loans);
 
 // Account details
 app.post("/account_details", getAccountDetails);
-/*app.post("/create_fixed_deposit", createFixedDeposit);*/
+app.post("/create_fixed_deposit", createFixedDeposit);
 
 // Existing routes...
 app.get("/", (req, res) => {
